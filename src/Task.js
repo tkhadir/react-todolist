c8e05a42e29ed1bd584633efc6d888c98c724bc5
tkhadir <tkhadir@nextoo.fr>
2019-12-26 23:48:03 +0100

    init tutorial

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import DateTimePicker from 'react-datetime-picker';
+import moment from 'moment';
+
 
 const EMPTY_FIELD = 0;
 var isAlreadyExist = function(items, field) {
@@ -12,17 +14,50 @@ var isAlreadyExist = function(items, field) {
     return exists;
 }
 
+var formateDate = function (date) {
+    return moment(date).format('DD/MM/YYYY HH:mm')
+  }
+
 class Task extends Component {
     state={
         element:"",
         items:[],
-        date: new Date()
+        date: new Date(),
+        description: '',
+        descriptionClassName: 'updateFormHidden'
     }
 
     onChange = (e) => {
         this.setState({
             [e.target.name]: e.target.value
         })
+    }
+
+    handleDescriptionClick = () => {
+        console.log('click')
+        this.setState({
+            descriptionClassName: this.state.descriptionClassName === 'updateFormHidden' ? 'updateFormVisible' : 'updateFormHidden'
+        })
+    }
+
+    handleUpdateSubmit = (e, index) => {
+        e.preventDefault()
+        if (e.target.value.length !== EMPTY_FIELD) {
+            //this.setState(state => {
+                /*const tasks = state.list.items((item, j) => {
+                  if (j === index) {
+                    item.element = e.target.value
+                    return item;
+                  } else {
+                    return item;
+                  }
+                });
+                return {
+                  tasks,
+                };*/
+            //});
+            console.log('update')
+        }
     }
 
     onDateChange = (updatedDate) => {
@@ -60,10 +95,16 @@ class Task extends Component {
                     <li key={index} className="taskItem">
                             <button className="deleteButton"
                             onClick={() => this.deleteItem(index)}>
-                                X
+                                Delete
                             </button>
-                            <span>{item.element + ' task '}</span>
-                            <span>{' scheduled at : ' + item.date}</span>
+                            <div>
+                                <span onClick={this.handleDescriptionClick} className="taskDesciption">{item.element}</span>
+                                <form className={this.state.descriptionClassName} onSubmit={() => this.handleUpdateSubmit(index)}>
+                                    <input type="text" name="description"></input>
+                                    <button>update</button>
+                                </form>
+                                <p>{'scheduled for : ' + formateDate(item.date)}</p>
+                            </div>
                     </li>
             ))}
         </ul>)
@@ -74,14 +115,16 @@ class Task extends Component {
             <React.Fragment>
             <div>
                 <div>
-                    <form onSubmit={this.onSubmit}>
+                    <form onSubmit={this.onSubmit} className="w3-container w3-card-4 w3-light-grey">
+                            <label className="w3-text-teal"><b>Task Name</b></label>
                             <input type="text" name="element"
-                            onChange={this.onChange} value={this.state.element} />
+                            onChange={this.onChange} value={this.state.element} 
+                            className="w3-input w3-border w3-round-large"/>
                             <DateTimePicker
                                 onChange={this.onDateChange}
                                 value={this.state.date}
-                                />
-                        <button>Add</button>
+                                className="w3-input w3-border w3-round-large"/>
+                        <button className="w3-btn w3-blue">Add</button>
                     </form>
                 </div>
             </div>
